9a6389566ae34e16b284f230cc75a5e9b558f652
magicmarie <natukunda162@gmail.com>
2019-09-15 20:18:05 +0300

    create array with objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert("Hello World!");
-
-var favFood = "pizza";
-document.write(favFood);
+var repository = [
+    pikachu = {
+        name : "Pikachu",
+        height : "0.4m",
+        types : [Static, Lightningrod]
+    } ,
+    nidorina = {
+        name : "Nidorina",
+        height : "0.8m",
+        types : [Poison-point, Hustle, Rivalry]
+    },
+    arcanine = {
+        name : "Arcanine",
+        height : "1.9m",
+        types : [Flash-fire, Intimidate, Justified]
+    }
+]
