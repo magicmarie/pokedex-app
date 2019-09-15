16e3ca211195fd9fa9dc8e1b733df8f86fe9ba22
magicmarie <natukunda162@gmail.com>
2019-09-15 20:57:34 +0300

    add if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
 var repository = [
     {
         name : "Pikachu",
-        height : "0.4m",
+        height : 0.4,
         types : ["Static", "Lightningrod"]
     } ,
     {
         name : "Nidorina",
-        height : "0.8m",
+        height : 0.8,
         types : ["Poison-point", "Hustle, Rivalry"]
     },
     {
         name : "Arcanine",
-        height : "1.9m",
+        height : 1.9,
         types : ["Flash-fire", "Intimidate", "Justified"]
     }
 ]
 
 for (i = 0; i < repository.length; i++ ) {
-    document.write('name:' + repository[i]['name'] + ' height: ' + repository[i]['height']);
+    if (repository[i]["height"] > 1.5)
+    document.write('<p>' + ' name: ' + repository[i]['name'] + ' height: ' + repository[i]['height'] + "wow that's big" + '</p>');
+    else
+    document.write('<p>' + ' name: ' + repository[i]['name'] + ' height: ' + repository[i]['height'] + '</p>');
 }
