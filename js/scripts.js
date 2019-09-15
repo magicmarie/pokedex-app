18aca34a1c6130624f4a24ed06a0cc2bd610238e
magicmarie <natukunda162@gmail.com>
2019-09-15 20:49:19 +0300

    add for-loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
 var repository = [
-    pikachu = {
+    {
         name : "Pikachu",
         height : "0.4m",
-        types : [Static, Lightningrod]
+        types : ["Static", "Lightningrod"]
     } ,
-    nidorina = {
+    {
         name : "Nidorina",
         height : "0.8m",
-        types : [Poison-point, Hustle, Rivalry]
+        types : ["Poison-point", "Hustle, Rivalry"]
     },
-    arcanine = {
+    {
         name : "Arcanine",
         height : "1.9m",
-        types : [Flash-fire, Intimidate, Justified]
+        types : ["Flash-fire", "Intimidate", "Justified"]
     }
 ]
+
+for (i = 0; i < repository.length; i++ ) {
+    document.write('name:' + repository[i]['name'] + ' height: ' + repository[i]['height']);
+}
